b1711492fb48a261e9c05d116eaf8412cd567367
Livar-Lende <273517@uis.no>
2024-10-02 16:55:01 +0200

    Update Oving_6_D.py

--- Oving_6_D.py
+++ Oving_6_D.py
@@ -1,22 +1,24 @@
 import datetime
 import matplotlib.pyplot as plt
 
-temperaturer_met= []
+temperaturer_met = []
 tider_met = []
-temperaturer =[]
+temperaturer = []
 tider = []
-
 
 with open("temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r") as fil:
     for linje in fil:
         Del = linje.strip().split(';')
-        if len(Del) >=5:
+        if len(Del) >= 5:
             tid = Del[2]
             temperatur = Del[3].replace(',', '.')
             try:
-                dato_obj = datetime.datetime.strptime(tid, "%m.%d.%Y %H:%M")
+                if "am" in tid or "pm" in tid:
+                    dato_obj = datetime.datetime.strptime(tid, "%m/%d/%Y %I:%M:%S %p")
+                else:
+                    dato_obj = datetime.datetime.strptime(tid, "%m.%d.%Y %H:%M")
+                
                 tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
-
                 temperatur_float = float(temperatur)
                 tider_met.append(tid_standard)
                 temperaturer_met.append(temperatur_float)
@@ -26,13 +28,13 @@ with open("temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r") as fil:
 with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r") as fil:
     for linje in fil:
         Del = linje.strip().split(';')
-        if len(Del) >=5:
+        if len(Del) >= 5:
             tid = Del[0]
             temperatur = Del[4].replace(',', '.')
             try:
                 dato_obj = datetime.datetime.strptime(tid, "%d.%m.%Y %H:%M")
+                
                 tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
-
                 temperatur_float = float(temperatur)
                 tider.append(tid_standard)
                 temperaturer.append(temperatur_float)
@@ -42,9 +44,13 @@ with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r") as fil:
 tider_met_dt = [datetime.datetime.strptime(tid, "%Y-%m-%d %H:%M:%S") for tid in tider_met]
 tider_dt = [datetime.datetime.strptime(tid, "%Y-%m-%d %H:%M:%S") for tid in tider]
 
+plt.figure(figsize=(10, 5))
 plt.plot(tider_met_dt, temperaturer_met, label="Måling fra Solas værstasjon")
 plt.plot(tider_dt, temperaturer, label="Måling fra UiS")
 plt.xlabel("Tid")
 plt.ylabel("Temperatur")
+plt.title("Temperaturmålinger fra to kilder")
 plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.show()
