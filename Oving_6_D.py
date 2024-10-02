94b2b26be6dba2ce83911a7d20b5df4c67901f3c
Livar-Lende <273517@uis.no>
2024-10-02 16:52:04 +0200

    Lagt kode til oppgave d og e (ikke ferdig)
    Dersom dere kjører koden ser dere at det er noe som ikke stemmer i grafene

---
+++ Oving_6_D.py
@@ -0,0 +1,50 @@
+import datetime
+import matplotlib.pyplot as plt
+
+temperaturer_met= []
+tider_met = []
+temperaturer =[]
+tider = []
+
+
+with open("temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r") as fil:
+    for linje in fil:
+        Del = linje.strip().split(';')
+        if len(Del) >=5:
+            tid = Del[2]
+            temperatur = Del[3].replace(',', '.')
+            try:
+                dato_obj = datetime.datetime.strptime(tid, "%m.%d.%Y %H:%M")
+                tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+                temperatur_float = float(temperatur)
+                tider_met.append(tid_standard)
+                temperaturer_met.append(temperatur_float)
+            except ValueError:
+                pass
+
+with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r") as fil:
+    for linje in fil:
+        Del = linje.strip().split(';')
+        if len(Del) >=5:
+            tid = Del[0]
+            temperatur = Del[4].replace(',', '.')
+            try:
+                dato_obj = datetime.datetime.strptime(tid, "%d.%m.%Y %H:%M")
+                tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+                temperatur_float = float(temperatur)
+                tider.append(tid_standard)
+                temperaturer.append(temperatur_float)
+            except ValueError:
+                pass
+
+tider_met_dt = [datetime.datetime.strptime(tid, "%Y-%m-%d %H:%M:%S") for tid in tider_met]
+tider_dt = [datetime.datetime.strptime(tid, "%Y-%m-%d %H:%M:%S") for tid in tider]
+
+plt.plot(tider_met_dt, temperaturer_met, label="Måling fra Solas værstasjon")
+plt.plot(tider_dt, temperaturer, label="Måling fra UiS")
+plt.xlabel("Tid")
+plt.ylabel("Temperatur")
+plt.legend()
+plt.show()
